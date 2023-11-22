0fee0559de240806daaa1c24614ade00f40d61d9
JOSE VARGAS <joseroberto-vargas@hotmail.com>
2023-11-22 15:22:50 -0500

    proyecto terminado

--- src/components/modales/ModalMantenimiento.jsx
+++ src/components/modales/ModalMantenimiento.jsx
@@ -10,7 +10,7 @@ const ModalMantenimiento = () => {
   const navigate = useNavigate();
 
   const goweb = () => {
-    navigate("/desarrollo");
+    navigate("/mantenimiento");
   };
 
   return (
--- src/pages/Clientes.jsx
+++ src/pages/Clientes.jsx
@@ -1,55 +1,77 @@
- import nuevoLatir from "../assets/ClienteNl.png" 
+import nuevoLatir from "../assets/ClienteNl.png";
 import TrabajosCarrusel from "../components/carrusel/Trabajosrecientes";
+import ReactPlayer from "react-player";
 
 const Section2Home = () => {
+  const vidUrl = "https://www.youtube.com/watch?v=hbxpjh4wvmo";
   return (
     <section className=" flex flex-col  p-10 mt-32 w-full  justify-center border-b border-border-color">
       <div className=" flex flex-col lg:flex-row  items-center lg:m-10 gap-20">
         <div className=" w-full   ">
-          
           <div className=" flex flex-col mt-3 m-2">
             <h3 className=" font-poppins font-semibold text-md lg:text-3xl">
               <span className=" text-btm-color">Nuestros clientes</span>
             </h3>
             <p className=" text-justify mt-3 text-md  ">
-            Ofrecemos a nuestros clientes la experiencia única de trabajos de calidad. Desde la conceptualización hasta la entrega final
+              Ofrecemos a nuestros clientes la experiencia única de trabajos de
+              calidad. Desde la conceptualización hasta la entrega final
             </p>
           </div>
         </div>
         <div className="  flex justify-center items-center -mt-7">
-          <img className=" mb-20 lg:mb-0 w-full lg:w-screen" src={nuevoLatir} alt="" />
+          <img
+            className=" mb-20 lg:mb-0 w-full lg:w-screen"
+            src={nuevoLatir}
+            alt=""
+          />
         </div>
       </div>
 
       <div className=" flex flex-col lg:flex-row  items-center lg:m-10 gap-20">
         <div className=" w-full   ">
-          
           <div className=" flex flex-col mt-3 m-2">
             <h3 className=" font-poppins font-semibold text-md lg:text-3xl">
               <span className=" text-btm-color">Trabajos recientes</span>
             </h3>
             <p className=" text-justify mt-3 text-md  ">
-            Recientemente, tuvimos el privilegio de trabajar en la optimización de la sala de sistemas de nuestro colegio asociado. Nuestro enfoque se centró en maximizar la eficiencia y la funcionalidad, asegurando que cada elemento tecnológico contribuyera al entorno educativo de manera significativa.
+              Recientemente, tuvimos el privilegio de trabajar en la
+              optimización de la sala de sistemas de nuestro colegio asociado.
+              Nuestro enfoque se centró en maximizar la eficiencia y la
+              funcionalidad, asegurando que cada elemento tecnológico
+              contribuyera al entorno educativo de manera significativa.
             </p>
           </div>
           <div>
-            <TrabajosCarrusel/>
+            <TrabajosCarrusel />
           </div>
-          <br /><br />
+          <br />
+          <br />
           <hr />
           <div>
             <strong>Experiencia de un cliente feliz 💣💣</strong>
             <br />
             <br />
 
-            <p>La satisfacción del cliente es nuestra mayor recompensa! En este video, <strong>Carlos Londoño</strong> profesor de primaria y usuario de los computadores de la sala restaurada comparte su experiencia con nuestros servicios.</p>
+            <p>
+              La satisfacción del cliente es nuestra mayor recompensa! En este
+              video, <strong>Carlos Londoño</strong> profesor de primaria y
+              usuario de los computadores de la sala restaurada comparte su
+              experiencia con nuestros servicios.
+            </p>
+            <br />
+            <br />
+          </div>
+          <div className=" flex justify-start w-80   lg:w-full">
+            <ReactPlayer
+              url={vidUrl}
+              playing={false}
+              volume={0.5}
+              controls={true}
+            />
           </div>
           <hr />
-          
         </div>
-        
       </div>
-
     </section>
   );
 };
